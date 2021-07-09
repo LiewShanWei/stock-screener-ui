6627222857cee16e50a7613d22cf407d389eacec
LiewShanWei <shanweiliew93@gmail.com>
2021-07-10 02:03:39 +0800

    Created Column.js file which contains column constants of the income statement, balance sheet and cash flow

---
+++ src/components/Finviz/Columns.js
@@ -0,0 +1,428 @@
+export const incomeStatementColumns = [
+    {
+        name: 'Ticker',
+        selector: 'ticker',
+        sortable: true
+    },
+    {
+        name: 'Period End',
+        selector: 'end_date',
+        sortable: false
+    },
+    {
+        name: 'Period Length',
+        selector: 'period_length',
+        sortable: false
+    },
+    {
+        name: 'Total Revenue',
+        selector: 'total_revenue',
+        sortable: true
+    },
+    {
+        name: 'Cost of Revenue',
+        selector: 'costof_revenue',
+        sortable: true
+    },
+    {
+        name: 'Gross Profit',
+        selector: 'gross_profit',
+        sortable: true
+    },
+    {
+        name: 'Selling/General/Admin',
+        selector: 'selling_general_admin',
+        sortable: true
+    },
+    {
+        name: 'R&D',
+        selector: 'research_development',
+        sortable: true
+    },
+    {
+        name: 'Unusual Expense/Income',
+        selector: 'unusual_exp_inc',
+        sortable: true
+    },
+    {
+        name: 'Total Operating Expense',
+        selector: 'total_operating_expense',
+        sortable: true
+    },
+    {
+        name: 'Operating Income',
+        selector: 'operating_income',
+        sortable: true
+    },
+    {
+        name: 'Interest Income Net',
+        selector: 'interest_income_net',
+        sortable: true
+    },
+    {
+        name: 'Other Income Net',
+        selector: 'other_income_net',
+        sortable: true
+    },
+    {
+        name: 'Net Income before Taxes',
+        selector: 'net_income_before_taxes',
+        sortable: true
+    },
+    {
+        name: 'Provision for Income Taxes',
+        selector: 'provision_income_taxes',
+        sortable: true
+    },
+    {
+        name: 'Net Income',
+        selector: 'net_income',
+        sortable: true
+    },
+    {
+        name: 'Income Avail to Common Excl Extraord',
+        selector: 'income_avail_common_excl_extr',
+        sortable: true
+    },
+    {
+        name: 'Income Avail to Common Incl Extraord',
+        selector: 'income_avail_common_incl_extr',
+        sortable: true
+    },
+    {
+        name: 'Diluted Average Shares',
+        selector: 'diluted_average_shares',
+        sortable: true
+    },
+    {
+        name: 'Diluted EPS Excl Extraord',
+        selector: 'diluted_eps_excl_extr',
+        sortable: true
+    },
+    {
+        name: 'Diluted EPS Incl. Extraord',
+        selector: 'diluted_eps_incl_extr',
+        sortable: true
+    }
+]
+
+export const balanceSheetColumns = [
+    {
+        name: 'Ticker',
+        selector: 'ticker',
+        sortable: true
+    },
+    {
+        name: 'Period End',
+        selector: 'end_date',
+        sortable: false
+    },
+    {
+        name: 'Cash and Equivalents',
+        selector: 'cash_eqv',
+        sortable: true
+    },
+    {
+        name: 'Short Term Investments',
+        selector: 'shortterm_invst',
+        sortable: true
+    },
+    {
+        name: 'Cash and Short Term Investments',
+        selector: 'cash_shortterm_invst',
+        sortable: true
+    },
+    {
+        name: 'Accounts Receivable - Trade, Net',
+        selector: 'account_receivable',
+        sortable: true
+    },
+    {
+        name: 'Total Receivables, Net',
+        selector: 'total_receivable',
+        sortable: true
+    },
+    {
+        name: 'Total Inventory',
+        selector: 'total_inventory',
+        sortable: true
+    },
+    {
+        name: 'Total Current Assets',
+        selector: 'total_current_asset',
+        sortable: true
+    },
+    {
+        name: 'Property, Plant And Equipment - Gross',
+        selector: 'ppe_gross',
+        sortable: true
+    },
+    {
+        name: 'Property, Plant And Equipment - Net',
+        selector: 'ppe_net',
+        sortable: true
+    },
+    {
+        name: 'Goodwill, Net',
+        selector: 'goodwill_net',
+        sortable: true
+    },
+    {
+        name: 'Intangibles, Net',
+        selector: 'intangible_net',
+        sortable: true
+    },
+    {
+        name: 'Long Term Investments',
+        selector: 'longterm_invst',
+        sortable: true
+    },
+    {
+        name: 'Other Long Term Assets',
+        selector: 'other_longterm_asset',
+        sortable: true
+    },
+    {
+        name: 'Total Assets',
+        selector: 'total_asset',
+        sortable: true
+    },
+    {
+        name: 'Accounts Payable',
+        selector: 'account_payable',
+        sortable: true
+    },
+    {
+        name: 'Accrued Expenses',
+        selector: 'accrued_expense',
+        sortable: true
+    },
+    {
+        name: 'Notes Payable/Short Term Debt',
+        selector: 'notes_payable_shortterm_deby',
+        sortable: true
+    },
+    {
+        name: 'Current Port. of LT Debt/Capital Leases',
+        selector: 'current_port_longterm_debt_capital_lease',
+        sortable: true
+    },
+    {
+        name: 'Other Current liabilities',
+        selector: 'other_current_liability',
+        sortable: true
+    },
+    {
+        name: 'Total Current Liabilities',
+        selector: 'total_current_liability',
+        sortable: true
+    },
+    {
+        name: 'Long Term Debt',
+        selector: 'longterm_debt',
+        sortable: true
+    },
+    {
+        name: 'Capital Lease Obligations',
+        selector: 'capital_lease_obligation',
+        sortable: true
+    },
+    {
+        name: 'Other Long Term Assets',
+        selector: 'other_longterm_asset',
+        sortable: true
+    },
+    {
+        name: 'Total Long Term Debt',
+        selector: 'total_longterm_debt',
+        sortable: true
+    },
+    {
+        name: 'Total Debt',
+        selector: 'total_debt',
+        sortable: true
+    },
+    {
+        name: 'Other Liabilities',
+        selector: 'other_liability',
+        sortable: true
+    },
+    {
+        name: 'Total Liabilities',
+        selector: 'total_liability',
+        sortable: true
+    },
+    {
+        name: 'Common Stock',
+        selector: 'common_stock',
+        sortable: true
+    },
+    {
+        name: 'Additional Paid-In Capital',
+        selector: 'add_paidin_capital',
+        sortable: true
+    },
+    {
+        name: 'Retained Earnings',
+        selector: 'retained_earning',
+        sortable: true
+    },
+    {
+        name: 'Unrealized Gain (Loss)',
+        selector: 'unrzl_gain_loss',
+        sortable: true
+    },
+    {
+        name: 'Other Equity',
+        selector: 'other_equity',
+        sortable: true
+    },
+    {
+        name: 'Total Equity',
+        selector: 'total_equity',
+        sortable: true
+    },
+    {
+        name: 'Total Liabilities and Equity',
+        selector: 'total_liability_equity',
+        sortable: true
+    },
+    {
+        name: 'Total Common Shares Outstanding',
+        selector: 'total_common_share_outstanding',
+        sortable: true
+    },
+    {
+        name: 'Full-Time Employees',
+        selector: 'full_time_employee',
+        sortable: true
+    },
+    {
+        name: 'Number of Common Shareholders',
+        selector: 'num_common_shareholder',
+        sortable: true
+    }
+]
+
+export const cashFlowColumns = [
+    {
+        name: 'Ticker',
+        selector: 'ticker',
+        sortable: true
+    },
+    {
+        name: 'Period End',
+        selector: 'end_date',
+        sortable: false
+    },
+    {
+        name: 'Period Length',
+        selector: 'period_length',
+        sortable: false
+    },
+    {
+        name: 'Net Income',
+        selector: 'net_income',
+        sortable: true
+    },
+    {
+        name: 'Depreciation/Depletion',
+        selector: 'depreciation_depletion',
+        sortable: true
+    },
+    {
+        name: 'Amortization',
+        selector: 'amortization',
+        sortable: true
+    },
+    {
+        name: 'Deferred Taxes',
+        selector: 'deferred_tax',
+        sortable: true
+    },
+    {
+        name: 'Non-Cash Items',
+        selector: 'non_cash_item',
+        sortable: true
+    },
+    {
+        name: 'Changes in Working Capital',
+        selector: 'changes_in_working_capital',
+        sortable: true
+    },
+    {
+        name: 'Cash from Operating Activities',
+        selector: 'cash_operating_activity',
+        sortable: true
+    },
+    {
+        name: 'Capital Expenditures',
+        selector: 'capital_expenditure',
+        sortable: true
+    },
+    {
+        name: 'Other Investing Cash Flow Items',
+        selector: 'other_invst_cashflow_item',
+        sortable: true
+    },
+    {
+        name: 'Cash from Investing Activities',
+        selector: 'cash_invst_activity',
+        sortable: true
+    },
+    {
+        name: 'Financing Cash Flow Items',
+        selector: 'finance_cashflow_item',
+        sortable: true
+    },
+    {
+        name: 'Total Cash Dividends Paid',
+        selector: 'total_cash_dividend_paid',
+        sortable: true
+    },
+    {
+        name: 'Issuance (Retirement) of Stock',
+        selector: 'issuance_of_stock',
+        sortable: true
+    },
+    {
+        name: 'Issuance (Retirement) of Debt',
+        selector: 'issuance_of_debt',
+        sortable: true
+    },
+    {
+        name: 'Cash from Financing Activities',
+        selector: 'cash_financing_activity',
+        sortable: true
+    },
+    {
+        name: 'Foreign Exchange Effects',
+        selector: 'foreign_exchange_effect',
+        sortable: true
+    },
+    {
+        name: 'Net Cash - Beginning Balance',
+        selector: 'net_cash_beginning_balance',
+        sortable: true
+    },
+    {
+        name: 'Net Cash - Ending Balance',
+        selector: 'net_cash_ending_balance',
+        sortable: true
+    },
+    {
+        name: 'Net Change in Cash',
+        selector: 'net_change_in_cash',
+        sortable: true
+    },
+    {
+        name: 'Cash Interest Paid',
+        selector: 'cash_interest_paid',
+        sortable: true
+    },
+    {
+        name: 'Cash Taxes Paid',
+        selector: 'cash_tax_paid',
+        sortable: true
+    }
+]
