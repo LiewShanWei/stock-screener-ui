41c4f4f62f9bd8ebbccfc578f49047f3d11ba2af
LiewShanWei <shanweiliew93@gmail.com>
2021-07-17 16:55:13 +0800

    Columns now compatible with selector syntax

--- src/components/Finviz/Columns.js
+++ src/components/Finviz/Columns.js
@@ -1,107 +1,107 @@
 export const incomeStatementColumns = [
     {
         name: 'Ticker',
-        selector: 'ticker',
+        selector: row => row['ticker'],
         sortable: true
     },
     {
         name: 'Period End',
-        selector: 'end_date',
+        selector: row => row['data']['Period End Date'][0],
         sortable: false
     },
     {
         name: 'Period Length',
-        selector: 'period_length',
+        selector: row => row['data']['Period Length'][0],
         sortable: false
     },
     {
         name: 'Total Revenue',
-        selector: 'total_revenue',
+        selector: row => row['data']['Period End Date'][0],
         sortable: true
     },
     {
         name: 'Cost of Revenue',
-        selector: 'costof_revenue',
+        selector: row => row['data']['Cost of Revenue'][0],
         sortable: true
     },
     {
         name: 'Gross Profit',
-        selector: 'gross_profit',
+        selector: row => row['data']['Gross Profit'][0],
         sortable: true
     },
     {
         name: 'Selling/General/Admin',
-        selector: 'selling_general_admin',
+        selector: row => row['data']['Selling, General and Administrative'][0],
         sortable: true
     },
     {
         name: 'R&D',
-        selector: 'research_development',
+        selector: row => row['data']['Research and Development'][0],
         sortable: true
     },
     {
         name: 'Unusual Expense/Income',
-        selector: 'unusual_exp_inc',
+        selector: row => row['data']['Unusual Expense/Income'][0],
         sortable: true
     },
     {
         name: 'Total Operating Expense',
-        selector: 'total_operating_expense',
+        selector: row => row['data']['Total Operating Expense'][0],
         sortable: true
     },
     {
         name: 'Operating Income',
-        selector: 'operating_income',
+        selector: row => row['data']['Operating Income'][0],
         sortable: true
     },
     {
         name: 'Interest Income Net',
-        selector: 'interest_income_net',
+        selector: row => row['data']['Interest Income Net'][0],
         sortable: true
     },
     {
         name: 'Other Income Net',
-        selector: 'other_income_net',
+        selector: row => row['data']['Other Income Net'][0],
         sortable: true
     },
     {
         name: 'Net Income before Taxes',
-        selector: 'net_income_before_taxes',
+        selector: row => row['data']['Net Income Before Taxes'][0],
         sortable: true
     },
     {
         name: 'Provision for Income Taxes',
-        selector: 'provision_income_taxes',
+        selector: row => row['data']['Provision for Income Taxes'][0],
         sortable: true
     },
     {
         name: 'Net Income',
-        selector: 'net_income',
+        selector: row => row['data']['Net Income'][0],
         sortable: true
     },
     {
         name: 'Income Avail to Common Excl Extraord',
-        selector: 'income_avail_common_excl_extr',
+        selector: row => row['data']['Income Avail. to Common Excl. Extraord.'][0],
         sortable: true
     },
     {
         name: 'Income Avail to Common Incl Extraord',
-        selector: 'income_avail_common_incl_extr',
+        selector: row => row['data']['Income Avail. to Common Incl. Extraord.'][0],
         sortable: true
     },
     {
         name: 'Diluted Average Shares',
-        selector: 'diluted_average_shares',
+        selector: row => row['data']['Diluted Average Shares'][0],
         sortable: true
     },
     {
         name: 'Diluted EPS Excl Extraord',
-        selector: 'diluted_eps_excl_extr',
+        selector: row => row['data']['Diluted EPS Excl. Extraord.'][0],
         sortable: true
     },
     {
         name: 'Diluted EPS Incl. Extraord',
-        selector: 'diluted_eps_incl_extr',
+        selector: row => row['data']['Diluted EPS Incl. Extraord.'][0],
         sortable: true
     }
 ]
@@ -109,197 +109,197 @@ export const incomeStatementColumns = [
 export const balanceSheetColumns = [
     {
         name: 'Ticker',
-        selector: 'ticker',
+        selector: row => row['ticker'],
         sortable: true
     },
     {
         name: 'Period End',
-        selector: 'end_date',
+        selector: row => row['data']['Period End Date'][0],
         sortable: false
     },
     {
         name: 'Cash and Equivalents',
-        selector: 'cash_eqv',
+        selector: row => row['data']['Cash and Equivalents'][0],
         sortable: true
     },
     {
         name: 'Short Term Investments',
-        selector: 'shortterm_invst',
+        selector: row => row['data']['Short Term Investments'][0],
         sortable: true
     },
     {
         name: 'Cash and Short Term Investments',
-        selector: 'cash_shortterm_invst',
+        selector: row => row['data']['Cash and Short Term Investments'][0],
         sortable: true
     },
     {
         name: 'Accounts Receivable - Trade, Net',
-        selector: 'account_receivable',
+        selector: row => row['data']['Accounts Receivable - Trade, Net'][0],
         sortable: true
     },
     {
         name: 'Total Receivables, Net',
-        selector: 'total_receivable',
+        selector: row => row['data']['Total Receivables, Net'][0],
         sortable: true
     },
     {
         name: 'Total Inventory',
-        selector: 'total_inventory',
+        selector: row => row['data']['Total Inventory'][0],
         sortable: true
     },
     {
         name: 'Total Current Assets',
-        selector: 'total_current_asset',
+        selector: row => row['data']['Total Current Assets'][0],
         sortable: true
     },
     {
         name: 'Property, Plant And Equipment - Gross',
-        selector: 'ppe_gross',
+        selector: row => row['data']['Property, Plant And Equipment - Gross'][0],
         sortable: true
     },
     {
         name: 'Property, Plant And Equipment - Net',
-        selector: 'ppe_net',
+        selector: row => row['data']['Property, Plant And Equipment - Net'][0],
         sortable: true
     },
     {
         name: 'Goodwill, Net',
-        selector: 'goodwill_net',
+        selector: row => row['data']['Goodwill, Net'][0],
         sortable: true
     },
     {
         name: 'Intangibles, Net',
-        selector: 'intangible_net',
+        selector: row => row['data']['Intangibles, Net'][0],
         sortable: true
     },
     {
         name: 'Long Term Investments',
-        selector: 'longterm_invst',
+        selector: row => row['data']['Long Term Investments'][0],
         sortable: true
     },
     {
         name: 'Other Long Term Assets',
-        selector: 'other_longterm_asset',
+        selector: row => row['data']['Other Long Term Assets'][0],
         sortable: true
     },
     {
         name: 'Total Assets',
-        selector: 'total_asset',
+        selector: row => row['data']['Total Assets'][0],
         sortable: true
     },
     {
         name: 'Accounts Payable',
-        selector: 'account_payable',
+        selector: row => row['data']['Accounts Payable'][0],
         sortable: true
     },
     {
         name: 'Accrued Expenses',
-        selector: 'accrued_expense',
+        selector: row => row['data']['Accrued Expenses'][0],
         sortable: true
     },
     {
         name: 'Notes Payable/Short Term Debt',
-        selector: 'notes_payable_shortterm_deby',
+        selector: row => row['data']['Notes Payable/Short Term Debt'][0],
         sortable: true
     },
     {
         name: 'Current Port. of LT Debt/Capital Leases',
-        selector: 'current_port_longterm_debt_capital_lease',
+        selector: row => row['data']['Current Port. of LT Debt/Capital'][0],
         sortable: true
     },
     {
         name: 'Other Current liabilities',
-        selector: 'other_current_liability',
+        selector: row => row['data']['Other Current liabilities'][0],
         sortable: true
     },
     {
         name: 'Total Current Liabilities',
-        selector: 'total_current_liability',
+        selector: row => row['data']['Total Current Liabilities'][0],
         sortable: true
     },
     {
         name: 'Long Term Debt',
-        selector: 'longterm_debt',
+        selector: row => row['data']['Long Term Debt'][0],
         sortable: true
     },
     {
         name: 'Capital Lease Obligations',
-        selector: 'capital_lease_obligation',
+        selector: row => row['data']['Capital Lease Obligations'][0],
         sortable: true
     },
     {
         name: 'Other Long Term Assets',
-        selector: 'other_longterm_asset',
+        selector: row => row['data']['Other Long Term Assets'][0],
         sortable: true
     },
     {
         name: 'Total Long Term Debt',
-        selector: 'total_longterm_debt',
+        selector: row => row['data']['Total Long Term Debt'][0],
         sortable: true
     },
     {
         name: 'Total Debt',
-        selector: 'total_debt',
+        selector: row => row['data']['Total Debt'][0],
         sortable: true
     },
     {
         name: 'Other Liabilities',
-        selector: 'other_liability',
+        selector: row => row['data']['Other Liabilities'][0],
         sortable: true
     },
     {
         name: 'Total Liabilities',
-        selector: 'total_liability',
+        selector: row => row['data']['Total Liabilities'][0],
         sortable: true
     },
     {
         name: 'Common Stock',
-        selector: 'common_stock',
+        selector: row => row['data']['Common Stock'][0],
         sortable: true
     },
     {
         name: 'Additional Paid-In Capital',
-        selector: 'add_paidin_capital',
+        selector: row => row['data']['Additional Paid-In Capital'][0],
         sortable: true
     },
     {
         name: 'Retained Earnings',
-        selector: 'retained_earning',
+        selector: row => row['data']['Retained Earnings'][0],
         sortable: true
     },
     {
         name: 'Unrealized Gain (Loss)',
-        selector: 'unrzl_gain_loss',
+        selector: row => row['data']['Unrealized Gain (Loss)'][0],
         sortable: true
     },
     {
         name: 'Other Equity',
-        selector: 'other_equity',
+        selector: row => row['data']['Other Equity'][0],
         sortable: true
     },
     {
         name: 'Total Equity',
-        selector: 'total_equity',
+        selector: row => row['data']['Total Equity'][0],
         sortable: true
     },
     {
         name: 'Total Liabilities and Equity',
-        selector: 'total_liability_equity',
+        selector: row => row['data']['Total Liabilities and Equity'][0],
         sortable: true
     },
     {
         name: 'Total Common Shares Outstanding',
-        selector: 'total_common_share_outstanding',
+        selector: row => row['data']['Total Common Shares Outstanding'][0],
         sortable: true
     },
     {
         name: 'Full-Time Employees',
-        selector: 'full_time_employee',
+        selector: row => row['data']['Full-Time Employees'][0],
         sortable: true
     },
     {
         name: 'Number of Common Shareholders',
-        selector: 'num_common_shareholder',
+        selector: row => row['data']['Number of Common Shareholders'][0],
         sortable: true
     }
 ]
@@ -307,122 +307,122 @@ export const balanceSheetColumns = [
 export const cashFlowColumns = [
     {
         name: 'Ticker',
-        selector: 'ticker',
+        selector: row => row['ticker'],
         sortable: true
     },
     {
         name: 'Period End',
-        selector: 'end_date',
+        selector: row => row['data']['Period End Date'][0],
         sortable: false
     },
     {
         name: 'Period Length',
-        selector: 'period_length',
+        selector: row => row['data']['Period Length'][0],
         sortable: false
     },
     {
         name: 'Net Income',
-        selector: 'net_income',
+        selector: row => row['data']['Net Income'][0],
         sortable: true
     },
     {
         name: 'Depreciation/Depletion',
-        selector: 'depreciation_depletion',
+        selector: row => row['data']['Depreciation/Depletion'][0],
         sortable: true
     },
     {
         name: 'Amortization',
-        selector: 'amortization',
+        selector: row => row['data']['Amortization'][0],
         sortable: true
     },
     {
         name: 'Deferred Taxes',
-        selector: 'deferred_tax',
+        selector: row => row['data']['Deferred Taxes'][0],
         sortable: true
     },
     {
         name: 'Non-Cash Items',
-        selector: 'non_cash_item',
+        selector: row => row['data']['Non-Cash Items'][0],
         sortable: true
     },
     {
         name: 'Changes in Working Capital',
-        selector: 'changes_in_working_capital',
+        selector: row => row['data']['Changes in Working Capital'][0],
         sortable: true
     },
     {
         name: 'Cash from Operating Activities',
-        selector: 'cash_operating_activity',
+        selector: row => row['data']['Cash from Operating Activities'][0],
         sortable: true
     },
     {
         name: 'Capital Expenditures',
-        selector: 'capital_expenditure',
+        selector: row => row['data']['Capital Expenditures'][0],
         sortable: true
     },
     {
         name: 'Other Investing Cash Flow Items',
-        selector: 'other_invst_cashflow_item',
+        selector: row => row['data']['Other Investing Cash Flow Items'][0],
         sortable: true
     },
     {
         name: 'Cash from Investing Activities',
-        selector: 'cash_invst_activity',
+        selector: row => row['data']['Cash from Investing Activities'][0],
         sortable: true
     },
     {
         name: 'Financing Cash Flow Items',
-        selector: 'finance_cashflow_item',
+        selector: row => row['data']['Financing Cash Flow Items'][0],
         sortable: true
     },
     {
         name: 'Total Cash Dividends Paid',
-        selector: 'total_cash_dividend_paid',
+        selector: row => row['data']['Total Cash Dividends Paid'][0],
         sortable: true
     },
     {
         name: 'Issuance (Retirement) of Stock',
-        selector: 'issuance_of_stock',
+        selector: row => row['data']['Issuance (Retirement) of Stock'][0],
         sortable: true
     },
     {
         name: 'Issuance (Retirement) of Debt',
-        selector: 'issuance_of_debt',
+        selector: row => row['data']['Issuance (Retirement) of Debt'][0],
         sortable: true
     },
     {
         name: 'Cash from Financing Activities',
-        selector: 'cash_financing_activity',
+        selector: row => row['data']['Cash from Financing Activities'][0],
         sortable: true
     },
     {
         name: 'Foreign Exchange Effects',
-        selector: 'foreign_exchange_effect',
+        selector: row => row['data']['Foreign Exchange Effects'][0],
         sortable: true
     },
     {
         name: 'Net Cash - Beginning Balance',
-        selector: 'net_cash_beginning_balance',
+        selector: row => row['data']['Net Cash - Beginning Balance'][0],
         sortable: true
     },
     {
         name: 'Net Cash - Ending Balance',
-        selector: 'net_cash_ending_balance',
+        selector: row => row['data']['Net Cash - Ending Balance'][0],
         sortable: true
     },
     {
         name: 'Net Change in Cash',
-        selector: 'net_change_in_cash',
+        selector: row => row['data']['Net Change in Cash'][0],
         sortable: true
     },
     {
         name: 'Cash Interest Paid',
-        selector: 'cash_interest_paid',
+        selector: row => row['data']['Cash Interest Paid'][0],
         sortable: true
     },
     {
         name: 'Cash Taxes Paid',
-        selector: 'cash_tax_paid',
+        selector: row => row['data']['Cash Taxes Paid'][0],
         sortable: true
     }
 ]
