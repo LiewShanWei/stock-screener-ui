640b9c19fe0248cd309ec1f1d5a7f7ddb32c1d93
LiewShanWei <shanweiliew93@gmail.com>
2021-08-14 23:25:12 +0800

    Added compact styling for PerformanceColumn

--- src/components/Finviz/Columns/PerformanceColumns.js
+++ src/components/Finviz/Columns/PerformanceColumns.js
@@ -10,82 +10,98 @@ const performanceColumns = [
     {
         name: 'Ticker',
         selector: row => row['ticker'],
-        sortable: true
+        sortable: true,
+        maxWidth: "100px"
     },
     {
         name: 'Period End[0]',
         selector: row => row[quarterCashFlow]['data']['Period End Date'][0],
-        sortable: false
+        sortable: false,
+        compact: true
     },
     {
         name: 'Period End[1]',
         selector: row => row[quarterCashFlow]['data']['Period End Date'][1],
-        sortable: false
+        sortable: false,
+        compact: true
     },
     {
         name: 'Period End[2]',
         selector: row => row[quarterCashFlow]['data']['Period End Date'][2],
-        sortable: false
+        sortable: false,
+        compact: true
     },
     {
         name: 'Total Revenue[0]',
         selector: row => row[quarterIncomeStatement]['data']['Total Revenue'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Revenue[1]',
         selector: row => row[quarterIncomeStatement]['data']['Total Revenue'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Revenue[2]',
         selector: row => row[quarterIncomeStatement]['data']['Total Revenue'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Income[0]',
         selector: row => row[quarterCashFlow]['data']['Net Income'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Income[1]',
         selector: row => row[quarterCashFlow]['data']['Net Income'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Income[2]',
         selector: row => row[quarterCashFlow]['data']['Net Income'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Current Assets[0]',
         selector: row => row[quarterBalanceSheet]['data']['Total Current Assets'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Current Assets[1]',
         selector: row => row[quarterBalanceSheet]['data']['Total Current Assets'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Current Assets[2]',
         selector: row => row[quarterBalanceSheet]['data']['Total Current Assets'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Assets[0]',
         selector: row => row[quarterBalanceSheet]['data']['Total Assets'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Assets[1]',
         selector: row => row[quarterBalanceSheet]['data']['Total Assets'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Assets[2]',
         selector: row => row[quarterBalanceSheet]['data']['Total Assets'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Liabilities[0]',
@@ -95,72 +111,86 @@ const performanceColumns = [
     {
         name: 'Total Liabilities[1]',
         selector: row => row[quarterBalanceSheet]['data']['Total Liabilities'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Total Liabilities[2]',
         selector: row => row[quarterBalanceSheet]['data']['Total Liabilities'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Cash from Operating[0]',
         selector: row => row[quarterCashFlow]['data']['Cash from Operating Activities'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Cash from Operating[1]',
         selector: row => row[quarterCashFlow]['data']['Cash from Operating Activities'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Cash from Operating[2]',
         selector: row => row[quarterCashFlow]['data']['Cash from Operating Activities'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Cash from Investing[0]',
         selector: row => row[quarterCashFlow]['data']['Cash from Investing Activities'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Cash from Investing[1]',
         selector: row => row[quarterCashFlow]['data']['Cash from Investing Activities'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Cash from Investing[2]',
         selector: row => row[quarterCashFlow]['data']['Cash from Investing Activities'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Cash - Ending[0]',
         selector: row => row[quarterCashFlow]['data']['Net Cash - Ending Balance'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Cash - Ending[1]',
         selector: row => row[quarterCashFlow]['data']['Net Cash - Ending Balance'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Cash - Ending[2]',
         selector: row => row[quarterCashFlow]['data']['Net Cash - Ending Balance'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Change in Cash[0]',
         selector: row => row[quarterCashFlow]['data']['Net Change in Cash'][0],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Change in Cash[1]',
         selector: row => row[quarterCashFlow]['data']['Net Change in Cash'][1],
-        sortable: true
+        sortable: true,
+        compact: true
     },
     {
         name: 'Net Change in Cash[2]',
         selector: row => row[quarterCashFlow]['data']['Net Change in Cash'][2],
-        sortable: true
+        sortable: true,
+        compact: true
     }
 ]
 
