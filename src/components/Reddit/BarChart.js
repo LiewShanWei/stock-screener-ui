0ab7a628b7bf8aa540b8f5dfe35764700159cae9
LiewShanWei <shanweiliew93@gmail.com>
2021-11-13 20:05:09 +0800

    installed react-google-charts and created base BarChart.js

---
+++ src/components/Reddit/BarChart.js
@@ -0,0 +1,33 @@
+import Chart from "react-google-charts";
+
+const BarChart = (props) => {
+    const chartType = "BarChart";
+
+    const options = 
+    {
+        title: props.optionsTitle,
+        chartArea: { width:'50%'},
+        isStacked: props.isStacked,
+        hAxis: {
+            title: props.optionsHAxisTitle
+        },
+        vAxis: {
+            title: props.optionsVAxisTitle,
+            minValue: 0
+        }
+    };
+
+    return (
+        <Chart
+            width = {props.width}
+            height = {props.height}
+            chartType = {chartType}
+            isStacked={props.isStacked}
+            loader = {<div>Loading Chart...</div>}
+            data = {props.data}
+            options = {options}
+        />
+    );
+};
+
+export default BarChart;
